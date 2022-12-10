a5fb97ba16c9dad86e4eb883311af045523c2fa9
ansavanix <55422282+ansavanix@users.noreply.github.com>
2022-12-09 20:38:08 -0800

    Implement place()

--- Board.cpp
+++ Board.cpp
@@ -16,7 +16,7 @@ class Morabaraba {
 	bool over;
 	char winner;
 	char cows[2];
-	bool millCheck(const int& x, const int& y);
+	char millCheck(const int& x, const int& y); //Returns how many mills have been created after a movement or placement at a certain position (0 - 2)
 	bool inBounds(const int& x, const int& y); //Returns if a position is in the bounds of a board.
 	public:
 	Morabaraba() {
@@ -37,9 +37,9 @@ class Morabaraba {
 	bool getTurn();
 	bool isOver();
 	bool validPlacement(const int& x, const int& y); //Check if it is valid to place a cow at (x,y).
-	bool place(const int& x, const int& y); //Actually place a cow. Returns true if placement creates a mill.
+	bool place(const int& x, const int& y); //Actually place a cow. Returns how many mills were created.
 	bool validMovement(const int& originalX, const int& originalY, const int& finalX, const int& finalY); //Check if a move is valid, returns true if it is.
-	bool move(const int& originalX, const int& originalY, const int& finalX, const int& finalY); //Actually move a cow from (originalX, originalY) to (finalX, finalY). Returns true if movement creates a mill.
+	char move(const int& originalX, const int& originalY, const int& finalX, const int& finalY); //Actually move a cow from (originalX, originalY) to (finalX, finalY). Returns how many mills were created.
 	void print();
 	
 }
@@ -65,13 +65,11 @@ bool Morabaraba::validPlacement(const int& x, const int& y) {
 	return false;
 }
 
-bool Morabaraba::place(const int& x, const int& y) {
+char Morabaraba::place(const int& x, const int& y) {
 	board[x][y] = turn;
 	cows[turn]++;
-	if (millCheck(x, y)) {
-		
-	}
 	turn = !turn;
+	return millCheck(x, y);
 }
 
 bool Morabaraba::validMovement(const int& originalX, const int& originalY, const int& finalX, const int& finalY) {
